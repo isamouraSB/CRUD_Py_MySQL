db74df1f526c5291570d47d779b807e4b1affa09
Isadora Moura <isadora-conceicao@hotmail.com>
2023-03-26 01:53:24 -0300

    crud para consulta

--- crud.py
+++ crud.py
@@ -6,28 +6,43 @@ conexao = mysql.connector.connect(
     password="2511",
     database="bd_crud",
 )
-
 cursor = conexao.cursor()
 
-#CRUD - CREATE
-Nome_Produto = "BATATA"
-Descricao_Produto = "PURE"
-Fornecedor_Produto = "AMBEV"
-Estoque_Produto = "5"
-Valor_Produto = "15,50"
-
-comando = 'INSERT INTO produtos (Nome_Produto, Descricao_Produto, Fornecedor_Produto, Estoque_Produto, Valor_Produto) VALUES ("{Nome_Produto}", "{Descricao_Produto}", "{Fornecedor_Produto}", "{Estoque_Produto}", "{Valor_Produto}")' # Escreve o comando 1
-cursor.execute(comando) #Executar o comando 2
-conexao.commit() #Edita o banco de dados 
-#resultado = cursor.fetchall() #Ler\armazenar o banco de dados 
-
+#CRUD ALTERAÇÕES
 
 
 #Fechando a conexão
 cursor.close()
 conexao.close()
 
+# CREATE\ CRIAR
+Nome_produtos = "Banana"
+Descr_produtos = "Pure de batata preparada com muito amor"
+Fornecedor_produtos = "AMBEV"
+Estoque_produtos = 5
+Valor_produtos = 15
+comando = f'INSERT INTO produtos (Nome_produtos, Descr_produtos, Fornecedor_produtos, Estoque_produtos, Valor_produtos) VALUES ("{Nome_produtos}", "{Descr_produtos}", "{Fornecedor_produtos}", "{Estoque_produtos}", "{Valor_produtos}")'
+cursor.execute(comando) #Executar o comando 2
+conexao.commit() #Edita o banco de dados 
+
 
 # READ
+comando = f'SELECT * FROM produtos' # Solicitar exibição dos resultados 
+cursor.execute(comando) #Executar o comando 2
+resultado = cursor.fetchall() #Ler\armazenar o banco de dados 
+print(resultado) #Exibir o resultado
+
+
 # UPDATE
+Cod_produtos = 2
+Valor_produtos = 12
+comando = f'UPDATE produtos SET Valor_produtos = {Valor_produtos} WHERE Cod_produtos = "{Cod_produtos}"' # Alterar valores da tabela
+cursor.execute(comando) #Executar o comando 2
+conexao.commit() #Edita o banco de dados 
+
+
 # DELETE
+Cod_produtos = 3
+comando = f'DELETE FROM produtos WHERE Cod_produtos = "{Cod_produtos}"' # Deletar linha de tabela
+cursor.execute(comando) #Executar o comando 2
+conexao.commit() #Edita o banco de dados 
